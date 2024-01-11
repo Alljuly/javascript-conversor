3df98a542b6669c36bf3244413b034b25a83946a
alice <ajsc1@aluno.ifal.edu.brm>
2024-01-11 20:26:50 -0300

    style: add css

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
-//valor de entrada
 let valor = document.getElementById("valor");
-
-//qual tipo de conversao
-let moedas = document.getElementsByName("moeda");
-
-//saida
+let moedas = document.getElementsByName("currency");
 let res = document.querySelector(".resultado");
 
 moedas.forEach((moeda) => {
-	moeda.addEventListener("click", conversao);
+	moeda.addEventListener("change", conversao);
 });
 
 valor.addEventListener("input", conversao);
 
+valor.addEventListener("focus", function () {
+	if (valor.value === "0.00") {
+		valor.value = "";
+	}
+	conversao;
+});
+valor.addEventListener("blur", function () {
+	if (valor.value === "") {
+		valor.value = "0.00";
+	} else {
+		valor.value = parseFloat(valor.value).toFixed(2);
+	}
+	conversao();
+});
+
 function conversao() {
-	let valorDigitado = parseFloat(valor.value);
+	var valorDigitado = parseFloat(valor.value);
+
 	moedas.forEach((moeda) => {
 		if (moeda.checked) {
 			if (moeda.value === "US") {
@@ -27,9 +38,9 @@ function conversao() {
 }
 
 function dolar(dado) {
-	return dado * 4.83;
+	return (dado * 4.83).toFixed(2);
 }
 
 function euro(dado) {
-	return dado * 5.83;
-} 
+	return (dado * 5.83).toFixed(2);
+}
